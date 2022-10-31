c06c2953047915451266a6eb00b7f57fa83121d9
khai <khaitruong1301@gmail.com>
2022-10-31 21:33:28 +0700

    buoi_1_2_create_component_databinding_state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './components/Card';
+import Demo from './components/Demo';
+import HomeLayout from './components/HomeLayout/HomeLayout';
+import DataBinding from './databinding/DataBinding';
+import HandleEvent from './handleEvent/HandleEvent';
+import DemoLogin from './State/DemoLogin';
+//import css: Ảnh hưởng toàn bộ ứng dụng
+import './index.scss';
+import TangGiamFontSize from './State/TangGiamFontSize/TangGiamFontSize';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//JSX 
+root.render(
+  <>
+    {/* <HomeLayout /> */}
+    {/* <DataBinding /> */}
+    {/* <HandleEvent /> */}
+    {/* <DemoLogin /> */}
+    <TangGiamFontSize />
+  </>
+  
+)
